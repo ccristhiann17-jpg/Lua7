a90b61830c542147fa98560f3b9597725de26a72
ccristhiann17-jpg <ccristhiann.17@gmail.com>
2026-05-31 20:32:50 -0600

    Create NEXAHUB SCRIPT DE PAGAv1.lua

---
+++ NEXAHUB SCRIPT DE PAGAv1.lua
@@ -0,0 +1,8 @@
+print("NEXA HUB by cristhian")
+
+-- =============================================
+--              NEXA HUB
+--         by cristhian
+-- =============================================
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lastxvc/-1-speed-keyboard/refs/heads/main/script"))()
