98b4b60ed67a625c8dfb8239f5516a17ae39789b
EL035 <ccristhiann.17@gmail.com>
2026-09-19 17:05:49 -0600

    Delete NEXAHUB SCRIPT DE PAGAv1.lua

--- NEXAHUB SCRIPT DE PAGAv1.lua
+++
@@ -1,8 +0,0 @@
-print("NEXA HUB by cristhian")
-
--- =============================================
---              NEXA HUB
---         by cristhian
--- =============================================
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lastxvc/-1-speed-keyboard/refs/heads/main/script"))()
